8844868c4abfa941df843d0fed09d195065b6339
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-01-31 22:24:27 +0900

    Add firebase setting

---
+++ src/containers/App/firebase-config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyAVl-ZLt9ZOTswpsEouUie3BtAGAIUcLIA",
+  authDomain: "react-posts-e7209.firebaseapp.com",
+  databaseURL: "https://react-posts-e7209.firebaseio.com",
+  projectId: "react-posts-e7209",
+  storageBucket: "react-posts-e7209.appspot.com",
+  messagingSenderId: "651936936344",
+  appId: "1:651936936344:web:c82b9456ab334f244abbed",
+  measurementId: "G-D03FCNG60S"
+};
